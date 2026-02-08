87a2eba6e160f2381dfd4c2442dea96c9c5d68ac
Priyanshu Chaudhary <priyanshuc2425@gmail.com>
2026-02-08 19:48:09 +0530

    some edit

--- ARRAY_REPRESENTATION/CODES/2darray.py
+++ ARRAY_REPRESENTATION/CODES/2darray.py
@@ -1,7 +1,7 @@
-matrix = []
+matrix = [[0]*3 for _ in range(3)]
 
 for i in range(3):
     for j in range(3):
-        matrix[i][j] = 3
+        matrix[i][j] = int(input(f"Please enter value for {i}{j}: "))
 
-print(matrix)
+print(f"{matrix}", end="\n")
